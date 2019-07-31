bd6cc1e69a18f4dae415a2b0477802a0bb017fa9
Nick Codignotto <ncodignotto@liquidnet.com>
2019-07-31 15:00:46 -0400

    Added example indexing

--- day3/api-skeleton.js
+++ day3/api-skeleton.js
@@ -11,6 +11,10 @@ let cells = [ EMPTY_CELL, EMPTY_CELL, EMPTY_CELL,
 //
 // Cell Manipulation - Translates row,col into flat index to access "cells" array
 //
+// [ 0  1  2
+//   3  4  5
+//   6  7  8 ]
+//
 
 function getCellValue(row, col) {
    // IMPLEMENT ME!
